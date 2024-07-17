c0b9472928078b165f3f24493696e22fe85ea613
JHORFENCASADIEGOS <129458344+JHORFENCASADIEGOS@users.noreply.github.com>
2024-07-17 11:23:52 -0500

    version 1.0.2

--- withLove/src/com/withlove/DAO/PatientDAOImpl.java
+++ withLove/src/com/withlove/DAO/PatientDAOImpl.java
@@ -9,6 +9,7 @@ import com.withlove.db.connection;
 import com.withlove.model.Patient;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -24,21 +25,24 @@ import javax.swing.JOptionPane;
  */
 public class PatientDAOImpl extends connection implements patientDAO {
 
+    
    
     @Override
     public void create(Patient pat)  {
         try {
             this.establishConnection();
-            PreparedStatement st = this.conect.prepareStatement("INSERT INTO patient (identification, firstNames, surNames, neighborhood, address, city, email, phoneNumber, password)  VALUES(?,?,?,?,?,?,?,?,?);");
-            st.setInt(1, pat.getIdentification());
-            st.setString(2, pat.getFirstNames());
-            st.setString(3, pat.getSurNames());
-            st.setString(4, pat.getNeighborhood());
-            st.setString(5, pat.getAddress());
-            st.setString(6, pat.getCity());
-            st.setString(7, pat.getEmail());
-            st.setString(8, pat.getPhoneNumber());
-            st.setString(9, pat.getPassword());
+            
+            PreparedStatement st = this.conect.prepareStatement("INSERT INTO patient (identification, namePatient, lastNamePa, email, phoneNumberPatient, birthdayDate, gender, passwordPatient, insurer)  VALUES(?,?,?,?,?,?,?,?,?);");
+            st.setString(1, pat.getIdentification());
+            st.setString(2, pat.getNamePatient());
+            st.setString(3, pat.getLastNamePa());
+            st.setString(4, pat.getEmail());
+            st.setString(5, pat.getPhoneNumberPatient());
+            java.sql.Date sqlDate = java.sql.Date.valueOf(pat.getBirthdayDate());
+            st.setDate(6, sqlDate);
+            st.setInt(7, pat.getGender());
+            st.setString(8, pat.getPasswordPatient());
+            st.setString(9, pat.getInsurer());
             st.executeUpdate();
             st.close();
         } catch(Exception e) {
@@ -51,29 +55,33 @@ public class PatientDAOImpl extends connection implements patientDAO {
             }
         }
     }
+    
+    
 
     @Override
-    public Patient searchForId(int id) {
+    public Patient searchForId(String id) {
         Patient pat = null;
         
         try {
             this.establishConnection();
             PreparedStatement st = this.conect.prepareStatement("SELECT * FROM patient WHERE identification = ? LIMIT 1;");
-            st.setInt(1, id);
+            st.setString(1, id);
             
             ResultSet rs = st.executeQuery();
             while(rs.next()) {
                 pat = new Patient();
-                pat.setIdentification(rs.getInt("identification"));
-                pat.setIdPatient(rs.getInt("idPatient"));
-                pat.setFirstNames(rs.getString("firstNames"));
-                pat.setSurNames(rs.getString("surNames"));
-                pat.setNeighborhood(rs.getString("neighborhood"));
-                pat.setAddress(rs.getString("address"));
-                pat.setCity(rs.getString("city"));
+                pat.setIdPatient(rs.getLong("idPatient"));
+                pat.setIdentification(rs.getString("identification"));
+                pat.setNamePatient(rs.getString("namePatient"));
+                pat.setLastNamePa(rs.getString("lastNamePa"));
                 pat.setEmail(rs.getString("email"));
-                pat.setPhoneNumber(rs.getString("phoneNumber"));
-                pat.setPassword(rs.getString("password"));
+                pat.setPhoneNumberPatient(rs.getString("phoneNumberPatient"));
+                LocalDate birthdayDate = rs.getDate("birthdayDate").toLocalDate();
+                pat.setBirthdayDate(birthdayDate);
+                pat.setGender(rs.getInt("gender"));
+                pat.setPasswordPatient(rs.getString("passwordPatient"));
+                pat.setInsurer(rs.getString("insurer"));
+                
             }
             rs.close();
             st.close();
@@ -88,23 +96,35 @@ public class PatientDAOImpl extends connection implements patientDAO {
         }
         return pat;
     }
-
+     /*     
+    private Long idPatient;
+    private String identification;
+    private String namePatient;
+    private String lastNamePa;
+    private String email;
+    private String phoneNumberPatient;
+    private LocalDate birthdayDate;
+    private Integer gender;
+    private String passwordPatient;
+    private String insurer;
+    */
     @Override
     public void update(Patient pat) {
          try {
             this.establishConnection();
-            PreparedStatement st = this.conect.prepareStatement("UPDATE patient SET firstNames = ?, surNames = ?, neighborhood = ?, address = ?, city = ?, email = ?, phoneNumber = ?, password = ? WHERE identification = ?;");
+            PreparedStatement st = this.conect.prepareStatement("UPDATE patient SET namePatient = ?, lastNamePa = ?, email = ?, phoneNumberPatient = ?, birthdayDate = ?,  gender = ?, passwordPatient = ?, insurer = ? WHERE identification = ?;");
             
 
-            st.setString(1, pat.getFirstNames());
-            st.setString(2, pat.getSurNames());
-            st.setString(3, pat.getNeighborhood());
-            st.setString(4, pat.getAddress());
-            st.setString(5, pat.getCity());
-            st.setString(6, pat.getEmail());
-            st.setString(7, pat.getPhoneNumber());
-            st.setString(8, pat.getPassword());
-            st.setInt(9, pat.getIdentification());
+            st.setString(1, pat.getNamePatient());
+            st.setString(2, pat.getLastNamePa());
+            st.setString(3, pat.getEmail());
+            st.setString(4, pat.getPhoneNumberPatient());
+            java.sql.Date sqlDate = java.sql.Date.valueOf(pat.getBirthdayDate());
+            st.setDate(5, sqlDate);
+            st.setInt(6, pat.getGender());
+            st.setString(7, pat.getPasswordPatient());
+            st.setString(8, pat.getInsurer());
+            st.setString(9, pat.getIdentification());
             st.executeUpdate();
             st.close();
         } catch(Exception e) {
@@ -119,11 +139,11 @@ public class PatientDAOImpl extends connection implements patientDAO {
     }
 
     @Override
-    public void delete(int id) {
+    public void delete(Long id) {
         try {
             this.establishConnection();
-            PreparedStatement st = this.conect.prepareStatement("DELETE FROM patient WHERE identification = ?;");
-            st.setInt(1, id);
+            PreparedStatement st = this.conect.prepareStatement("DELETE FROM patient WHERE idPatient = ?;");
+            st.setLong(1, id);
             st.executeUpdate();
             st.close();
         } catch(Exception e) {
@@ -144,21 +164,21 @@ public class PatientDAOImpl extends connection implements patientDAO {
         return mather.find();
     }
 
-    
+   
     @Override
     public Patient ValidatePatient(int id, String pass)  {
          Patient pat = null;
         
         try {
             this.establishConnection();
-            PreparedStatement st = this.conect.prepareStatement("SELECT firstNames,license FROM patient WHERE identification = ? AND password = ? LIMIT 1;");
+            PreparedStatement st = this.conect.prepareStatement("SELECT idPatient,namePatient FROM patient WHERE identification = ? AND passwordPatient = ? LIMIT 1;");
             st.setInt(1, id);
             st.setString(2, pass);
             ResultSet rs = st.executeQuery();
             while(rs.next()) {
-                pat = new Patient();
-                pat.setFirstNames(rs.getString("firstNames"));
-                pat.setLicense(rs.getInt("license"));
+                pat = new Patient();               
+                pat.setIdPatient(rs.getLong("idPatient"));
+                pat.setNamePatient(rs.getString("namePatient"));
                 JOptionPane.showConfirmDialog(null, "Welcome");
             }
             rs.close();
@@ -174,32 +194,5 @@ public class PatientDAOImpl extends connection implements patientDAO {
              }
         }
         return pat;
-    }
-
-    @Override
-    public void updateLicense(int license,int id) {
-        try {
-            this.establishConnection();
-            PreparedStatement st = this.conect.prepareStatement("UPDATE patient SET license = ? WHERE identification = ?;");
-            
-
-            st.setInt(1, license);
-            st.setInt(2, id);
-            
-            st.executeUpdate();
-            st.close();
-        } catch(Exception e) {
-            JOptionPane.showConfirmDialog(null, "Error: "+e.toString());
-        } finally {
-            try {
-                this.closeConnection();
-            } catch (SQLException ex) {
-                Logger.getLogger(PatientDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }    
-    }
-}
-
-   
-    
-
+    }   
+}  
